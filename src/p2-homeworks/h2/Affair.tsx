afa9dcc8983e77298870b0f7fd9140feddb3f08d
Maxim Remez <remezmaxim1992@gmail.com>
2022-07-11 14:35:09 +0300

    HW 2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import {AffairType} from "./HW2";
+import classes from "./Affairs.module.css";
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = () => {
+        props.deleteAffairCallback(props.affair._id)
+    }// need to fix
+
+    const priorityClass = classes.name + ' ' + classes[props.affair.priority]
 
     return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
+        <div className={classes.affair}>
+            <div className={classes.name}>{props.affair.name}</div>
+            <div className={priorityClass}>{props.affair.priority}</div>
+            <button className={classes.name + ' ' + classes.button} onClick={deleteCallback}>X</button>
         </div>
     )
 }
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import classes from "./Affairs.module.css";
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType>
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
+    filter: FilterType
 }
 
 function Affairs(props: AffairsPropsType) {
@@ -13,24 +15,35 @@ function Affairs(props: AffairsPropsType) {
         <Affair // should work
             key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
             affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
+            deleteAffairCallback={() =>props.deleteAffairCallback(a._id)}
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
-
+    const setAll = () => {
+        props.setFilter("all")
+    } // need to fix
+    const setHigh = () => {
+        props.setFilter("high")
+    }
+    const setMiddle = () => {
+        props.setFilter("middle")
+    }
+    const setLow = () => {
+        props.setFilter("low")
+    }
+    const cnAll = classes.button + ' ' + (props.filter === "all" ? classes.active : '')
+    const cnHigh = classes.button + ' ' + (props.filter === "high" ? classes.active : '')
+    const cnMiddle = classes.button + ' ' + (props.filter === "middle" ? classes.active : '')
+    const cnLow = classes.button + ' ' + (props.filter === "low" ? classes.active : '')
     return (
         <div>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button className = {cnAll} onClick={setAll}>All</button>
+            <button className = {cnHigh} onClick={setHigh}>High</button>
+            <button className = {cnMiddle} onClick={setMiddle}>Middle</button>
+            <button className = {cnLow} onClick={setLow}>Low</button>
         </div>
     )
 }
