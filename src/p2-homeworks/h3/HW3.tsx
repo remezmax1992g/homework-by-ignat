2dd5a4ba67828bb3a0c4d78cb447e04f2da382a9
Maxim Remez <remezmaxim1992@gmail.com>
2022-07-15 13:34:18 +0300

    HW 3 updated1

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -13,8 +13,7 @@ function HW3() {
     const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
     const addUserCallback = (name: string) => {
-        let id = v1();
-        setUsers([...users, {_id: id, name: name}]) // need to fix
+        setUsers([...users, {_id: v1(), name: name}]) // need to fix
     }
 
     return (
