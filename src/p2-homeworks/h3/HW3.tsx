991867dfd6fb682e85a6a44d92e811ac5d61a252
Maxim Remez <remezmaxim1992@gmail.com>
2022-07-25 16:35:36 +0300

    HW 4 updated

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -13,7 +13,8 @@ function HW3() {
     const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
 
     const addUserCallback = (name: string) => {
-        setUsers([...users, {_id: v1(), name: name}]) // need to fix
+        setUsers([...users, {_id: v1(), name: name}])
+        // need to fix
     }
 
     return (
--- src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/p2-homeworks/h4/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -20,7 +20,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // сделайте так чтоб работал onChange и onChangeChecked
+        onChange && onChange(e)
+        onChangeChecked && onChangeChecked(e.currentTarget.checked)// сделайте так чтоб работал onChange и onChangeChecked
     }
 
     const finalInputClassName = `${s.checkbox} ${className ? className : ''}`
